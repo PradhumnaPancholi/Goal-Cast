f4bd4d5d8014e2e9fe62bbfa0e73c294185b821c
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-08-05 17:40:51 -0400

    added pop-up to create task

---
+++ GoalCast/CreateNewTaskVC.swift
@@ -0,0 +1,30 @@
+//
+//  CreateNewTaskVC.swift
+//  GoalCast
+//
+//  Created by Pradhumna Pancholi on 2019-08-04.
+//  Copyright © 2019 Pradhumna Pancholi. All rights reserved.
+//
+
+import UIKit
+
+class CreateNewTaskVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
